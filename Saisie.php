c4d94c3c3e6de61ac0a2e50522c3e551d493d755
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-27 10:22:21 +0100

    fin 2- cas particulier formulaire caché etc.

--- Saisie.php
+++ Saisie.php
@@ -1,10 +1,21 @@
 <?php
-if (isset($_POST['nom1'])&& !empty($_POST)) {
-    echo $_POST['nom1'];
+/*$personne=array($nom,$prenom)
+
+if (isset($_POST['saisie[prenom]'])&& !empty($_POST['saisie[prenom]'])) {
+    foreach($_POST as $index=>$valeur){
+        echo $index;
+    }
 }
-elseif (isset($_POST['nom2'])&& !empty($_POST)) {
+/*elseif (isset($_POST['nom2'])&& !empty($_POST)) {
     echo $_POST['nom2'];
+}*/
+if (isset($_POST['nom'])&& !empty($_POST)) {
+    /*foreach($_POST as $index=>$valeur){*/
+        echo $_POST['nom'];
+    /*}*/
 }
 else {
     echo "Nom inconnu";
-}
+} 
+
+
