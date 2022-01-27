314a86a481628d470b418348e45772b8ff9cce77
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-27 11:15:38 +0100

    ajout affichage nom, mdp, sexe, photo couleurs, langue

--- Saisie.php
+++ Saisie.php
@@ -19,3 +19,56 @@ if (isset($_POST['saisie[prenom]'])&& !empty($_POST['saisie[prenom]'])) {
 // } 
 
 //--- Début Exercice
+//Affichage Nom
+if (isset($_POST['nom'])&& !empty($_POST['nom'])) {
+    echo "nom= ".$_POST['nom'];
+} else {
+    echo "Nom inconnu";
+}
+echo "<br>";
+//Affichage mot de passe
+
+if (isset($_POST['mot_de_passe'])&& !empty($_POST['mot_de_passe'])) {
+    echo "mdp= ".$_POST['mot_de_passe'];
+} else {
+    echo "Mdp inconnu";
+}
+echo "<br>";
+//Affichage sexe
+if (isset($_POST['sexe'])&& !empty($_POST['sexe'])) {
+    echo "mdp= ".$_POST['sexe'];
+} else {
+    echo "sexe inconnu";
+}
+echo "<br>";
+//Affichage fichier
+if (isset($_POST['photo'])&& !empty($_POST['photo'])) {
+    echo "photo= ".$_POST['photo'];
+} else {
+    echo "photo introuvable";
+}
+echo "<br>";
+//Affichage couleurs
+function couleurCafe(){
+  foreach ($_POST['couleurs'] as $key => $value) {
+        echo $key." = ".$value." ";
+    }  
+};
+if (isset($_POST['couleurs'])&& !empty($_POST['couleurs'])) { 
+    echo "couleurs: ";
+    echo "<br>";
+    couleurCafe();   
+} 
+else {
+    echo "couleur non selectionnée";
+}
+echo "<br>";
+//Affichage couleurs
+if (isset($_POST['langue'])&& !empty($_POST['langue'])) { 
+    echo "langue= ".$_POST['langue'];   
+} 
+else {
+    echo "couleur non selectionnée";
+}
+echo "<br>";
+//Affichage couleurs
